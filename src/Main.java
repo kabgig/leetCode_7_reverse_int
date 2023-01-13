254775468a17ab552c78df6096d55e987f00cd69
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-13 18:49:50 +0400

    3 solution learn

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.reverse(-43403000));
+        Solution2 solution = new Solution2();
+        System.out.println(solution.reverse(-120));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -5,29 +5,26 @@ import java.util.stream.Collectors;
 class Solution {
     public int reverse(int x) {
         boolean isNegative = x < 0;
-        boolean canAdd = false;
         if (x == 0) return 0;
-        if (-Math.pow(2, 31) >= x || x >= Math.pow(2, 31) - 1) return 0;
-
 
         if (isNegative) x = Math.abs(x);
         while (x % 10 == 0) x /= 10;
-
         String number = Integer.toString(x);
-
         ArrayList<Character> newNumber = new ArrayList<>();
 
         for (int i = number.length() - 1; i >= 0; i--) {
             newNumber.add(number.charAt(i));
         }
 
-        String betweenStr = newNumber.stream().map(Objects::toString)
+        String betweenStr = newNumber
+                .stream()
+                .map(Objects::toString)
                 .collect(Collectors.joining(""));
         if (isNegative) betweenStr = "-" + betweenStr;
 
-        int res1 = Integer.parseInt(betweenStr);
+        int res = Integer.parseInt(betweenStr);
 
-        if (-Math.pow(2, 31) <= res1 || res1 <= Math.pow(2, 31) - 1) return res1;
+        if (-Math.pow(2, 31) <= res || res <= Math.pow(2, 31) - 1) return res;
         else return 0;
     }
 }
